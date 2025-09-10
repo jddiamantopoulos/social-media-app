2f65fd30475d612f19146d0b72f2ec5f1fa9ffdf
JonathanDiamantopoulos <jidiaman@asu.edu>
2025-09-10 00:46:12 -0700

    fix: added axios dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 export default defineConfig({
   plugins: [react()],
